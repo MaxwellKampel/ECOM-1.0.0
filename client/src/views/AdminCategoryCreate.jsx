56d18359577057332fe5fc5736f9db328af5d376
MaxwellKampel <maxwell.kampel@gmail.com>
2024-06-27 13:31:24 -0400

    Admin Views file creation

---
+++ client/src/views/AdminCategoryCreate.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AdminCategoryCreate = () => {
+  return (
+    <div>AdminCategoryCreate</div>
+  )
+}
+
+export default AdminCategoryCreate
---
+++ client/src/views/AdminProductsCreate.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AdminProductsCreate = () => {
+  return (
+    <div>AdminProductsCreate</div>
+  )
+}
+
+export default AdminProductsCreate
